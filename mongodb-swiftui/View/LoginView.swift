fc5a19b9b76604cc69a5a5192b7ed5d4e085c72b
ameysunu <ameysunu00719@gmail.com>
2021-07-14 14:18:02 +0530

    login ui improved

--- mongodb-swiftui/View/LoginView.swift
+++ mongodb-swiftui/View/LoginView.swift
@@ -13,61 +13,71 @@ struct LoginView: View {
     @State private var password: String = ""
     
     var body: some View {
-        VStack(alignment: .leading) {
+        NavigationView {
+            VStack(alignment: .leading) {
+                HStack {
+                    Spacer()
+                    Image("Login")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 350)
+                    Spacer()
+                }
 
-            Text("Login")
-                .font(.title)
-                .padding(.bottom, 10)
-            
-            TextField("Email ID",text: $email)
-                .padding()
-                .overlay(RoundedRectangle(cornerRadius: 5.0)
-                            .stroke(lineWidth: 1.0))
-                .padding(.bottom, 20)
-                .autocapitalization(.none)
-            
-            SecureField("Password", text: $password)
-                .padding()
-                .overlay(RoundedRectangle(cornerRadius: 5.0)
-                            .stroke(lineWidth: 1.0))
-            Button(action: {
                 
-            }) {
-                Text("Login")
-                    .padding(8)
-                    .frame(maxWidth: .infinity)
-                    .foregroundColor(.white)
-                    .padding(10)
-                    .overlay(RoundedRectangle(cornerRadius: 10)
-                                .stroke(lineWidth: 2.0)
-                                .shadow(color: .blue, radius: 10.0))
-                    .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
+                TextField("Email ID",text: $email)
+                    .padding()
+                    .overlay(RoundedRectangle(cornerRadius: 5.0)
+                                .stroke(lineWidth: 1.0))
+                    .padding(.bottom, 20)
+                    .autocapitalization(.none)
+                
+                SecureField("Password", text: $password)
+                    .padding()
+                    .overlay(RoundedRectangle(cornerRadius: 5.0)
+                                .stroke(lineWidth: 1.0))
+                
+                Button(action: {
+                    
+                }) {
+                    Text("Login")
+                        .padding(8)
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(.white)
+                        .padding(10)
+                        .overlay(RoundedRectangle(cornerRadius: 10)
+                                    .stroke(lineWidth: 2.0)
+                                    .shadow(color: .blue, radius: 10.0))
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
+                }
+                .padding(.top, 10)
+                
+                HStack {
+                    Color.gray.frame(height:CGFloat(1) / UIScreen.main.scale)
+                    Text("or")
+                        .font(.title3)
+                        .foregroundColor(.gray)
+                    Color.gray.frame(height:CGFloat(1) / UIScreen.main.scale)
+                }
+                
+                Button(action: {
+                    
+                }) {
+                    Text("Login Anonymously")
+                        .padding(8)
+                        .frame(maxWidth: .infinity)
+                        .foregroundColor(.white)
+                        .padding(10)
+                        .overlay(RoundedRectangle(cornerRadius: 10)
+                                    .stroke(lineWidth: 2.0)
+                                    .shadow(color: .blue, radius: 10.0))
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
+                }
+                
             }
-            .padding(.top, 10)
-            
-            HStack {
-                Spacer()
-                Text("OR")
-                Spacer()
-            }
+            .navigationTitle("Login")
             .padding()
-            
-            Button(action: {
-                
-            }) {
-                Text("Login Anonymously")
-                    .padding(8)
-                    .frame(maxWidth: .infinity)
-                    .foregroundColor(.white)
-                    .padding(10)
-                    .overlay(RoundedRectangle(cornerRadius: 10)
-                                .stroke(lineWidth: 2.0)
-                                .shadow(color: .blue, radius: 10.0))
-                    .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
-            }
-
         }
-        .padding()
     }
 }
 
