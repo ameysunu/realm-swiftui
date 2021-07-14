1a52b90c396a32ab018c77b0894b835f12e64ada
ameysunu <ameysunu00719@gmail.com>
2021-07-14 11:09:38 +0530

    initial commit

---
+++ mongodb-swiftui/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  mongodb-swiftui
+//
+//  Created by Amey Sunu on 14/07/21.
+//
+
+import SwiftUI
+import RealmSwift
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mongodb-swiftui/mongodb_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  mongodb_swiftuiApp.swift
+//  mongodb-swiftui
+//
+//  Created by Amey Sunu on 14/07/21.
+//
+
+import SwiftUI
+
+@main
+struct mongodb_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
