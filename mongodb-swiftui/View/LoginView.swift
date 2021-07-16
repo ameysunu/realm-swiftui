b02d9eab7440df371fb410085f8e5d2b8d5eda09
ameysunu <ameysunu00719@gmail.com>
2021-07-16 22:31:56 +0530

    login improved

--- mongodb-swiftui/View/LoginView.swift
+++ mongodb-swiftui/View/LoginView.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 struct LoginView: View {
     
     @State private var email: String = ""
     @State private var password: String = ""
     @State var isNavigated: Bool = false
+    @State var isSignedBefore: Bool = false
     @State var load: Bool = false
     @State var anyload: Bool = false
     
@@ -33,48 +35,56 @@ struct LoginView: View {
                     .overlay(RoundedRectangle(cornerRadius: 5.0)
                                 .stroke(lineWidth: 1.0))
                 
-                NavigationLink(destination: WelcomeView().navigationBarBackButtonHidden(true), isActive: $isNavigated) {
-                    Button(action: {
-                        self.load.toggle()
-                        RealmAuth(email: email, password: password){
-                            (success) -> Void in
-                            if success {
-                                self.isNavigated = true
-                                self.load.toggle()
-                            }else {
-                                self.load.toggle()
-                                self.alertItem = AlertItem(title: Text("Error"), message: Text(e!), dismissButton: .default(Text("Done")))
+                NavigationLink(destination: HomeView().navigationBarBackButtonHidden(true), isActive: $isSignedBefore)  {
+                    NavigationLink(destination: WelcomeView().navigationBarBackButtonHidden(true), isActive: $isNavigated) {
+                        Button(action: {
+                            self.load.toggle()
+                            RealmAuth(email: email, password: password){
+                                (success) -> Void in
+                                if success {
+                                    self.load.toggle()
+                                    let userCheck = try! Realm().objects(UserData.self).filter("userID = '\(uid!)'")
+                                    if(userCheck.isEmpty){
+                                        self.isNavigated = true
+                                    } else {
+                                        self.isSignedBefore = true
+                                        print(userCheck)
+                                    }
+                                }else {
+                                    self.load.toggle()
+                                    self.alertItem = AlertItem(title: Text("Error"), message: Text(e!), dismissButton: .default(Text("Done")))
+                                }
                             }
-                        }
-                    }) {
-                        if(load == true){
-                            HStack {
-                                Spacer()
-                                ProgressView()
-                                Spacer()
-                            }
+                        }) {
+                            if(load == true){
+                                HStack {
+                                    Spacer()
+                                    ProgressView()
+                                    Spacer()
+                                }
+                                .padding(8)
+                                .frame(maxWidth: .infinity)
+                                .foregroundColor(.white)
+                                .padding(10)
+                                .overlay(RoundedRectangle(cornerRadius: 10)
+                                            .stroke(lineWidth: 2.0)
+                                            .shadow(color: .gray, radius: 10.0))
+                                .background(RoundedRectangle(cornerRadius: 10).fill(Color.gray))
+                                .disabled(true)
+                            } else {
+                                Text("Login")
                                     .padding(8)
                                     .frame(maxWidth: .infinity)
                                     .foregroundColor(.white)
                                     .padding(10)
                                     .overlay(RoundedRectangle(cornerRadius: 10)
                                                 .stroke(lineWidth: 2.0)
-                                                .shadow(color: .gray, radius: 10.0))
-                                    .background(RoundedRectangle(cornerRadius: 10).fill(Color.gray))
-                            .disabled(true)
-                        } else {
-                        Text("Login")
-                            .padding(8)
-                            .frame(maxWidth: .infinity)
-                            .foregroundColor(.white)
-                            .padding(10)
-                            .overlay(RoundedRectangle(cornerRadius: 10)
-                                        .stroke(lineWidth: 2.0)
-                                        .shadow(color: .blue, radius: 10.0))
-                            .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
+                                                .shadow(color: .blue, radius: 10.0))
+                                    .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
+                            }
+                        }
+                        .padding(.top, 10)
                     }
-                    }
-                    .padding(.top, 10)
                 }
                 
                 HStack {
@@ -85,7 +95,7 @@ struct LoginView: View {
                     Color.gray.frame(height:CGFloat(1) / UIScreen.main.scale)
                 }
                 
-                NavigationLink(destination: WelcomeView().navigationBarBackButtonHidden(true), isActive: $isNavigated) {
+                NavigationLink(destination: HomeView().navigationBarBackButtonHidden(true), isActive: $isNavigated) {
                     Button(action: {
                         RealmAuthAnonymous(){
                             (success) -> Void in
@@ -104,26 +114,26 @@ struct LoginView: View {
                                 ProgressView()
                                 Spacer()
                             }
-                                    .padding(8)
-                                    .frame(maxWidth: .infinity)
-                                    .foregroundColor(.white)
-                                    .padding(10)
-                                    .overlay(RoundedRectangle(cornerRadius: 10)
-                                                .stroke(lineWidth: 2.0)
-                                                .shadow(color: .gray, radius: 10.0))
-                                    .background(RoundedRectangle(cornerRadius: 10).fill(Color.gray))
-                            .disabled(true)
-                        } else {
-                        Text("Login Anonymously")
                             .padding(8)
                             .frame(maxWidth: .infinity)
                             .foregroundColor(.white)
                             .padding(10)
                             .overlay(RoundedRectangle(cornerRadius: 10)
                                         .stroke(lineWidth: 2.0)
-                                        .shadow(color: .blue, radius: 10.0))
-                            .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
-                    }
+                                        .shadow(color: .gray, radius: 10.0))
+                            .background(RoundedRectangle(cornerRadius: 10).fill(Color.gray))
+                            .disabled(true)
+                        } else {
+                            Text("Login Anonymously")
+                                .padding(8)
+                                .frame(maxWidth: .infinity)
+                                .foregroundColor(.white)
+                                .padding(10)
+                                .overlay(RoundedRectangle(cornerRadius: 10)
+                                            .stroke(lineWidth: 2.0)
+                                            .shadow(color: .blue, radius: 10.0))
+                                .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
+                        }
                     }
                 }
                 Spacer()
