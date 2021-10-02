239067ada972d50deeafe582c6d499feac95f6e9
ameysunu <ameysunu00719@gmail.com>
2021-10-02 11:58:30 +0100

    anonymous login removed

--- mongodb-swiftui/View/LoginView.swift
+++ mongodb-swiftui/View/LoginView.swift
@@ -86,56 +86,6 @@ struct LoginView: View {
                         .padding(.top, 10)
                     }
                 }
-                
-                HStack {
-                    Color.gray.frame(height:CGFloat(1) / UIScreen.main.scale)
-                    Text("or")
-                        .font(.title3)
-                        .foregroundColor(.gray)
-                    Color.gray.frame(height:CGFloat(1) / UIScreen.main.scale)
-                }
-                
-                NavigationLink(destination: HomeView().navigationBarBackButtonHidden(true), isActive: $isNavigated) {
-                    Button(action: {
-                        RealmAuthAnonymous(){
-                            (success) -> Void in
-                            if success {
-                                self.isNavigated = true
-                                self.anyload.toggle()
-                            } else {
-                                self.anyload.toggle()
-                                self.alertItem = AlertItem(title: Text("Error"), message: Text(e!), dismissButton: .default(Text("Done")))
-                            }
-                        }
-                    }) {
-                        if(anyload == true){
-                            HStack {
-                                Spacer()
-                                ProgressView()
-                                Spacer()
-                            }
-                            .padding(8)
-                            .frame(maxWidth: .infinity)
-                            .foregroundColor(.white)
-                            .padding(10)
-                            .overlay(RoundedRectangle(cornerRadius: 10)
-                                        .stroke(lineWidth: 2.0)
-                                        .shadow(color: .gray, radius: 10.0))
-                            .background(RoundedRectangle(cornerRadius: 10).fill(Color.gray))
-                            .disabled(true)
-                        } else {
-                            Text("Login Anonymously")
-                                .padding(8)
-                                .frame(maxWidth: .infinity)
-                                .foregroundColor(.white)
-                                .padding(10)
-                                .overlay(RoundedRectangle(cornerRadius: 10)
-                                            .stroke(lineWidth: 2.0)
-                                            .shadow(color: .blue, radius: 10.0))
-                                .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
-                        }
-                    }
-                }
                 Spacer()
                 HStack {
                     Spacer()
